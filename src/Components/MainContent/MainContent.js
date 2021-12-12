329311ed677d5873fc4d5371b692378b96eb36f3
ya-lybly-multiki <trifonov_artem@icloud.com>
2021-12-12 22:02:48 +0300

    minor edits

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -4,9 +4,9 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Particles from "react-particles-js";
-
 import { Fade } from "react-awesome-reveal"
 import Tilt from "react-tilt/dist/tilt";
+import ReactTypingEffect from 'react-typing-effect'
 
 const particlesParams = {
     particles: {
@@ -14,7 +14,7 @@ const particlesParams = {
             value: 'grey',
         },
         links: {
-            color: '#f1e8e8',
+            color: '#ffffff',
             distance: 150,
             enable: true,
             opacity: 0.5,
@@ -34,7 +34,7 @@ const particlesParams = {
                 enable: true,
                 value_area: 500,
             },
-            value: 150,
+            value: 200,
         },
         opacity: {
             value: 0.8,
@@ -50,11 +50,10 @@ export function MainContent() {
     return (
         <div id={"Main"} className={s.main}>
             <Particles options={particlesParams} className={s.particles}/>
-
             <div className={s.mainText}>
                 <span>Hi there</span>
                 <h1>I`M Artyom Trifonov.</h1>
-                <h3>I`m Fronted developer</h3>
+                <h2><ReactTypingEffect typingDelay={1500} eraseDelay={3000} text={'Frontend developer.'}/></h2>
                 <div className={s.mainLinks}>
                     <a target={"_blank"} href={"https://github.com/ya-lybly-multiki"}>
                     <GitHubIcon target={"_blank"} fontSize={"large"} />
@@ -62,14 +61,15 @@ export function MainContent() {
                     <a target={"_blank"} href={"https://t.me/Artem_Trifion"}>
                         <TelegramIcon target={"_blank"} fontSize={"large"} />
                     </a>
-                    <a target={"_blank"} href={"https://www.linkedin.com/in/%D0%B0%D1%80%D1%82%D1%91%D0%BC-%D1%82%D1%80%D0%B8%D1%84%D0%BE%D0%BD%D0%BE%D0%B2-9b6734225/"}>
+                    <a target={"_blank"}
+                       href={"https://www.linkedin.com/in/%D0%B0%D1%80%D1%82%D1%91%D0%BC-%D1%82%D1%80%D0%B8%D1%84%D0%BE%D0%BD%D0%BE%D0%B2-9b6734225/"}>
                         <LinkedInIcon target={"_blank"} fontSize={"large"} />
                     </a>
                 </div>
             </div>
             <Fade direction={'right'} triggerOnce><Tilt transitionSpeed={1600}>
-                <div className={s.mainPhotoWrapper}>
-                    <img className={s.mainPhoto}/>
+                <div className={s.mainPhoto}>
+                    <div className={s.mainPhotoWrapper}></div>
                 </div>
             </Tilt></Fade>
         </div>
